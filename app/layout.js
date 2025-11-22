7f8034e7ee9547dbfe2df626b6ece44fef576394
btcbarkeep <103902339+btcbarkeep@users.noreply.github.com>
2025-11-21 17:22:03 -1000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,13 @@ import "./globals.css";
 
 export const metadata = {
   title: "AinaReports",
-  description: "Reliable Property Reports for Hawaii"
+  description: "Reliable Condo Reports for Hawaii",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
