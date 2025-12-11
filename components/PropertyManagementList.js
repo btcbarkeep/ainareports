225ffe6479d3d5c815cbd0ae54aa068c0adf1366
btcbarkeep <103902339+btcbarkeep@users.noreply.github.com>
2025-12-10 14:44:54 -1000

    Update PropertyManagementList.js

--- components/PropertyManagementList.js
+++ components/PropertyManagementList.js
@@ -104,11 +104,11 @@ export default function PropertyManagementList({
             </div>
           </div>
         )}
-        <div className="w-2/5 min-w-0 pr-4 relative flex-shrink-0 flex items-center justify-center">
+        <div className="w-2/5 min-w-0 px-4 relative flex-shrink-0 flex items-center justify-center">
           {isPaid && (
             <span className="absolute -left-1.5 top-1/2 -translate-y-1/2 text-amber-500 text-[10px] leading-none">⭐</span>
           )}
-          <div className={`truncate overflow-hidden ${isPaid ? 'pl-3' : ''}`} title={pm.company_name || pm.name || "Property Manager"}>
+          <div className={`truncate overflow-hidden text-center w-full ${isPaid ? 'pl-3' : ''}`} title={pm.company_name || pm.name || "Property Manager"}>
             {pm.company_name || pm.name || "Property Manager"}
           </div>
         </div>
